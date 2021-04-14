e3ebf8da729def9b591c630ed865d9861aa9399a
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-04-14 14:03:08 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nadheer on 14/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
